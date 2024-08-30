c49f8f5223a938e1f9aff25721b59377a191052e
Sixten Peterson <sixten@snicon.rip>
2024-08-30 16:22:53 +0200

    Added more javadoc

--- src/main/java/rip/snicon/utils/SoundUtils.java
+++ src/main/java/rip/snicon/utils/SoundUtils.java
@@ -4,8 +4,34 @@ import net.kyori.adventure.sound.Sound;
 import net.minestom.server.sound.SoundEvent;
 import rip.snicon.Main;
 
+/**
+ * Utility class for converting strings to sound-related types.
+ *
+ * <p>This class provides methods to safely convert string inputs to
+ * {@link SoundEvent} and {@link Sound.Source} objects used in Minecraft server
+ * development. It handles invalid input gracefully by logging errors and
+ * returning default values.</p>
+ *
+ * @see SoundEvent
+ * @see Sound.Source
+ * @see <a href="https://javadoc.minestom.net/">Minestom Documentation</a>
+ * @see <a href="https://docs.adventure.kyori.net/">Adventure Documentation</a>
+ *
+ * @author znopp
+ */
 public class SoundUtils {
 
+    /**
+     * Converts a string to a {@link SoundEvent}.
+     *
+     * <p>This method attempts to convert the provided string, which represents a sound event's
+     * namespace ID, to a {@link SoundEvent} object. If the string is invalid or does not correspond
+     * to a valid sound event, the method logs an error and returns a default sound event
+     * ({@link SoundEvent#BLOCK_GRASS_BREAK}).</p>
+     *
+     * @param soundName The string representation of the sound event's namespace ID.
+     * @return A {@link SoundEvent} object corresponding to the provided string, or a default sound event if invalid.
+     */
     public static SoundEvent stringToSoundEvent(String soundName) {
         try {
             return SoundEvent.fromNamespaceId(soundName);
@@ -16,6 +42,16 @@ public class SoundUtils {
         }
     }
 
+    /**
+     * Converts a string to a {@link Sound.Source}.
+     *
+     * <p>This method attempts to convert the provided string to a {@link Sound.Source} enum value.
+     * If the string is invalid or does not correspond to a valid sound source, the method logs an
+     * error and returns a default sound source ({@link Sound.Source#MASTER}).</p>
+     *
+     * @param sourceName The string representation of the sound source.
+     * @return A {@link Sound.Source} corresponding to the provided string, or a default sound source if invalid.
+     */
     public static Sound.Source stringToSource(String sourceName) {
         try {
             return Sound.Source.valueOf(sourceName);
